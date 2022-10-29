ab489d9722704e0a2f282df97d818330d933d6bc
ptobiasdiaz <tobias.diaz@uclm.es>
2022-10-29 15:49:07 +0200

    Add distribution file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+'''Libreria de clientes para el proyecto RestFS'''
+
+from setuptools import setup
+
+setup(
+    name='restfs_client',
+    version='0.1',
+    description=__doc__,
+    packages=['restfs'],
+)
